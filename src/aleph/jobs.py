ea01abfa4df224db488e498befb3719a7ead10e9
Hugo Herter <git@hugoherter.com>
2020-12-02 18:24:13 +0100

    WIP: Better exception handling

--- src/aleph/jobs.py
+++ src/aleph/jobs.py
@@ -7,6 +7,7 @@ from typing import Coroutine, List
 import aioipfs
 import sentry_sdk
 from pymongo import DeleteOne, InsertOne, DeleteMany
+from pymongo.errors import CursorNotFound
 
 from aleph.chains.common import incoming, get_chaindata_messages
 from aleph.model.messages import Message
@@ -231,6 +232,8 @@ async def handle_txs_task():
         try:
             await handle_txs_job()
             await asyncio.sleep(5)
+        except CursorNotFound:
+            LOGGER.exception("Cursor error in pending txs job ")
         except Exception as e:
             LOGGER.exception("Error in pending txs job")
             sentry_sdk.capture_exception(e)
